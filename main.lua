7b5eb85a214179cec721c9fbe004d710f3b011b6
gb <giovanni.b.boff@gmail.com>
2025-03-07 21:05:29 -0800

    screen size, lua config, some shapes drawn

---
+++ main.lua
@@ -0,0 +1,10 @@
+function love.load()
+end
+
+function love.update()
+end
+
+function love.draw()
+    love.graphics.rectangle("fill", 400, 0, 300, 150)
+    love.graphics.polygon("fill", 100, 100, 200, 100, 150, 200)
+end
